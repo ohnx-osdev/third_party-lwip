f133741ff56fecd401aa7c8f439f5c000ae47efa
Toshi Kikuchi <toshik@google.com>
2016-10-03 10:27:44 -0700

    Enable DHCP
    Change-Id: Ia39e29f1da44cba9568359c4f741f9d8da79911b

--- ports/fuchsia/include/lwipopts.h
+++ ports/fuchsia/include/lwipopts.h
@@ -219,9 +219,9 @@ a lot of data that needs to be copied, this should be set high. */
 /* ---------- DHCP options ---------- */
 /* Define LWIP_DHCP to 1 if you want DHCP configuration of
    interfaces. */
-#define LWIP_DHCP               0
+#define LWIP_DHCP               1
 
-#define LWIP_DHCP_GET_NTP_SRV   (LWIP_DHCP)
+#define LWIP_DHCP_GET_NTP_SRV   0
 
 /* 1 if you want to do an ARP check on the offered address
    (recommended if using DHCP). */
