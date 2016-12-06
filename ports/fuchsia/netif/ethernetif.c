a99128c83b9b0131f58048bb3c27e35a0496a3e1
Tim Kilbourn <tkilbourn@google.com>
2016-12-06 10:15:17 -0800

    Fix printing of mac addresses
    On NUC, I was getting "ffffffb8:ffffffae:fffffffed...". Casting to
    uint8_t before printing does the right thing.
    
    Change-Id: If903b9b67d9af51722c0d214da7cc482e41def51

--- ports/fuchsia/netif/ethernetif.c
+++ ports/fuchsia/netif/ethernetif.c
@@ -223,7 +223,8 @@ low_level_input(struct netif *netif)
       memcpy(netif->hwaddr, buf, 6);
       printf("ethernetif: link up (mac %02x:%02x:%02x:%02x:%02x:%02x,"
              " mtu %u)\n",
-             buf[0], buf[1], buf[2], buf[3], buf[4], buf[5], mtu);
+             (uint8_t)buf[0], (uint8_t)buf[1], (uint8_t)buf[2], (uint8_t)buf[3],
+             (uint8_t)buf[4], (uint8_t)buf[5], mtu);
       netif_set_link_up(netif);
     } else {
       /* link down */
